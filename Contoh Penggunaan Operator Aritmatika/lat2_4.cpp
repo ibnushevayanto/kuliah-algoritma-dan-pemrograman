b19bddd03bc0f403aeb8300e9dfcba723abfe0d0
ibnushevayanto <ibnushevayanto@gmail.com>
2020-06-09 19:19:00 +0700

    Penambahan

---
+++ Contoh Penggunaan Operator Aritmatika/lat2_4.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+main()
+{
+ float luas, pi, jari;
+ 
+ pi = 3.1428;
+ jari = 21,4;
+ luas = pi*jari*jari;
+ 
+ printf ("luas = %f", luas);
+}
---
+++ Contoh Penggunaan Operator Pengerjaan/lat2_3.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+main()
+{
+int a,b,c;
+ a=33;
+ b=21;
+ c=a+b;
+ printf ("%d",c);
+}
+
