90714e1291129289f3c60445c9e05b2eee93cada
ibnushevayanto <ibnushevayanto@gmail.com>
2020-06-09 18:58:56 +0700

    First Commit

---
+++ Menghitung Luas dan Keliling Persegi Panjang/lat2_1.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+main()
+{
+	int panjang, lebar, luas, keliling;
+	
+	printf("Masukkan Panjang Persegi Panjang : ");
+	scanf ("%d", &panjang);
+	
+	printf("Masukkan Lebar Persegi Panjang : ");
+	scanf("%d")
+}
