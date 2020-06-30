5b4fb003494f61a01dd77d811d0130cecf8f8a65
unknown <ibnushevayanto@gmail.com>
2020-06-30 14:10:23 +0700

    Add Tugas Akhir

---
+++ String/latihan.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <stdlib.h>
+#include <cstring>
+
+using namespace std;
+
+void newLine(){
+	cout<<"\n";
+}
+
+int main(){
+	char nama[50], kelas[10], hobi[30];
+	
+	cout<<"Masukkan Nama : ";
+	cin.getline(nama, 50);
+	newLine();
+	
+	cout<<"Masukkan Kelas : ";
+	cin.getline(kelas, 10);
+	newLine();
+	
+	cout<<"Masukkan Hobi : ";
+	cin.getline(hobi, 30);
+	newLine();
+	
+	cout<<"Halo Nama Saya Adalah "<<nama<<", Saya Dari Kelas "<<kelas<<", Dan Hobi Saya Adalah "<<hobi;
+	
+}
